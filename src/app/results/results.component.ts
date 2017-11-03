5c023e41ff87704f013c98580860b532761fa4ca
Nicholas Rodman <rufio@Nicholass-MacBook-Pro.local>
2017-11-03 11:33:18 -0700

    adding API logos

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -17,9 +17,9 @@ import * as $ from 'jquery';
 export class ResultsComponent implements OnInit {
 
   //IMAGES
-  private eventRegistryLogo: string = '/assets/img/er-logo-1@2x.png';
-  private amChartsLogo: string = '/assets/img/am-charts.png';
-  private newsLogo: string = '/assets/img/newsLogo.jpg';
+  private eventRegistryLogo: string = 'https://pbs.twimg.com/profile_images/875698295808679936/b1Pqj1by_400x400.jpg';
+  private amChartsLogo: string = 'https://avatars0.githubusercontent.com/u/1116146?s=400&v=4';
+  private newsLogo: string = 'https://image.freepik.com/free-icon/news-logo_318-38132.jpg';
 
 
   //EVENT REGISTRY JSONS
