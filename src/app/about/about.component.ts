a774a883029362099ba28f219280d3eba4906057
Nicholas Rodman <gitnicholas@gmail.com>
2017-11-09 12:53:43 -0800

    added marketing copy, improved api brand formatting

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  
+
+  //IMAGES
+  private eventRegistryLogo: string = 'https://pbs.twimg.com/profile_images/875698295808679936/b1Pqj1by_400x400.jpg';
+  private amChartsLogo: string = 'https://avatars0.githubusercontent.com/u/1116146?s=400&v=4';
+  private newsLogo: string = 'https://image.freepik.com/free-icon/news-logo_318-38132.jpg';
+  private newsApiLogo: string = "https://pbs.twimg.com/profile_images/815237522641092609/6IeO3WLV.jpg";
 
   constructor() { }
 
